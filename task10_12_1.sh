1feafae359ef00bd451a3abaed70a263a4c633d4
SofyaTavrovskaya <tavrovskayasofya87@gmail.com>
2018-04-30 18:59:16 +0300

    add in script creation config drivers

--- task10_12_1.sh
+++ task10_12_1.sh
@@ -8,4 +8,13 @@ echo "Create directory for config-drives for vm1 and vm2"
 mkdir -p config-drives/$VM1_NAME-config config-drives/$VM2_NAME-config
 
 
+echo "Download Ubuntu cloud image, if it doesn't exist"
+wget -O /var/lib/libvirt/images/ubunut-server-16.04.qcow2 -nc https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img
 
+echo "Create two disks from image"
+cp /var/lib/libvirt/images/ubunut-server-16.04.qcow2 $VM1_HDD
+cp /var/lib/libvirt/images/ubunut-server-16.04.qcow2 $VM2_HDD
+
+echo "Create config drives"
+mkisofs -o "$VM1_CONFIG_ISO" -V cidata -r -J --quiet /config-drives/vm1-config
+mkisofs -o "$VM2_CONFIG_ISO" -V cidata -r -J --quiet /config-drives/vm2-config

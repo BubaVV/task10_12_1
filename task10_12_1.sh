2b2ebb507f8e855fb09a0e73b0df8b51886a9c7b
SofyaTavrovskaya <tavrovskayasofya87@gmail.com>
2018-05-03 00:13:27 +0300

    correct .sh file

--- task10_12_1.sh
+++ task10_12_1.sh
@@ -3,17 +3,21 @@
 echo "Export variables from config file"
 source config
 export $(cut -d= -f1 config| grep -v '^$\|^\s*\#' config)
+envsubst < config > work_config
+source work_config
+export $(cut -d= -f1 work_config| grep -v '^$\|^\s*\#' work_config)
+
 
 echo "Create directory for config-drives for vm1 and vm2"
 mkdir -p config-drives/$VM1_NAME-config config-drives/$VM2_NAME-config
 
 
 echo "Download Ubuntu cloud image, if it doesn't exist"
-wget -O /var/lib/libvirt/images/ubunut-server-16.04.qcow2 -nc https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img
+wget -O /var/lib/libvirt/images/ubuntu-server-16.04.qcow2 -nc https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img
 
 echo "Create two disks from image"
-cp /var/lib/libvirt/images/ubunut-server-16.04.qcow2 $VM1_HDD
-cp /var/lib/libvirt/images/ubunut-server-16.04.qcow2 $VM2_HDD
+cp /var/lib/libvirt/images/ubuntu-server-16.04.qcow2 $VM1_HDD
+cp /var/lib/libvirt/images/ubuntu-server-16.04.qcow2 $VM2_HDD
 
 echo "Create config drives"
 mkisofs -o "$VM1_CONFIG_ISO" -V cidata -r -J --quiet config-drives/vm1-config
@@ -29,6 +33,18 @@ then
 else
 	ssh-keygen -f $SSH_PUB_KEY -t rsa -b 4096
 fi
+
+echo "Create directory for libvirt network XMLs"
+mkdir -p networks/
+
+echo "Create external.xml from template"
+envsubst < templates/external_template.xml > networks/external.xml
+
+echo "Create internal.xml from template"
+envsubst < templates/internal_template.xml > networks/internal.xml
+
+echo "Create management.xml from template"
+envsubst < templates/management_template.xml > networks/management.xml
 
 
 
